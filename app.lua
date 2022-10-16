0c621ad5ca9bf2002b416cdb3051166e77ec337d
alexcoder04 <alexcoder04@protonmail.com>
2022-10-16 23:53:04 +0200

    solproj.yml

--- app.lua
+++ app.lua
@@ -1,10 +1,4 @@
 
-App.Name = "helloworld"
-App.Author = "alexcoder04"
-App.License = "GNU GPLv3"
-App.RefreshRate = 0.5
-App.SolVersion = 0
-
 App:AddElement(Components.Custom.HelloWorld:new())
 App:AddElement(Components.Custom.Clock:new())
 App:AddElement(Components.Custom.MyRectangle:new())
