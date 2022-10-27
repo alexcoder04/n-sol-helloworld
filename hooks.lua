8c24b1a2be8cd94bc7cbd038bfa63d76c01018d5
alexcoder04 <alexcoder04@protonmail.com>
2022-10-27 14:56:45 +0200

    new hooks api

--- hooks.lua
+++ hooks.lua
@@ -1,10 +1,10 @@
 
-function Hooks.Paint(gc)
+function App.Hooks.Paint(gc)
     gc:drawRect(100, 100, 20, 20)
     gc:drawString(tostring(App.Data.Var.Char), 100, 120, "top")
 end
 
-function Hooks.CharIn(c)
+function App.Hooks.CharIn(c)
     App.Data.Var.Char = c
     platform.window:invalidate()
 end
